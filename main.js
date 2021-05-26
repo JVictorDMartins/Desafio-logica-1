b246046d2b5a8620ce9c5d7f3988838b87983334
João Victor <jvictordmartins@gmail.com>
2021-05-26 16:50:25 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+// Crie uma variável que receba seu nome;
+
+// Crie uma variável que receba sua idade;
+
+// Crie uma variável com o ano atual e subtraia pela sua idade e mostre no console a mensagem "Meu ano de nascimento é...";
+
+// Crie uma variável chamada souhumano e atribua à ela o valor booleano que representa verdadeiro;
+
+// Crie duas variáveis e atribua a cada uma um número;
+
+// Agora mostre no console o resultado da soma das duas variáveis;
+
+
+let Nome = "João"
+let Idade = 21
+let Ano = 2021 - 21
+
+console.log(`Meu ano de nascimento é ${Ano}`);
+
+let souhumano = trues
+console.log(souhumano)
+
+let n1 = 17
+let n2 = 13
+console.log(n1 + n2)
